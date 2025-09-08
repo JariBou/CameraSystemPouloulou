7a8e72badb50654f294e8e3961775d25c19ca967
Jari Bou <bourdietome@gmail.com>
2025-09-08 11:27:00 +0200

    kdo bébou

---
+++ Assets/_project/Scripts/CameraConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace _project.Scripts
+{
+    [System.Serializable]
+    public struct CameraConfiguration
+    {
+        public float Yaw;
+        public float Pitch;
+        public float Roll;
+
+        public Vector3 Pivot;
+        public float Distance;
+        public float Fov;
+
+        public CameraConfiguration(float yaw, float pitch, float roll, Vector3 pivot, float distance, float fov)
+        {
+            Yaw = yaw;
+            Pitch = pitch;
+            Roll = roll;
+            Pivot = pivot;
+            Distance = distance;
+            Fov = fov;
+        }
+
+        Quaternion GetRotation()
+        {
+            return Quaternion.Euler(Pitch, Yaw, Roll);
+        }
+
+        Vector3 GetPosition()
+        {
+            return Pivot + GetRotation().eulerAngles * Distance; ;
+        }
+    }
+}
