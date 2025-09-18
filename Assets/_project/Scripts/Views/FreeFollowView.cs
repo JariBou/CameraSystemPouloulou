7ee78f8f0cd4cb4a75cb3570c169019793a6fcae
Jari Bou <bourdietome@gmail.com>
2025-09-18 09:55:13 +0200

    FreeFollowViewUpgrades

--- Assets/_project/Scripts/Views/FreeFollowView.cs
+++ Assets/_project/Scripts/Views/FreeFollowView.cs
@@ -18,6 +18,8 @@ namespace CameraSystem._project.Scripts.Views
         private GameObject _target;
 
         private Matrix4x4 _curveToWorldMatrix;
+        [SerializeField]
+        private bool _showOrbitCircles;
 
         internal Curve Curve => _curve;
         internal GameObject Target => _target;
@@ -79,11 +81,70 @@ namespace CameraSystem._project.Scripts.Views
             {
                 _curve.DrawGizmo(transform.localToWorldMatrix);
             }
+
+            if (!_showOrbitCircles || _curve.points.Count < 1) return;
+            
+            Gizmos.color = Color.yellow;
+            float tIncrement = 1f / (_curve.points.Count * 2f);
+            float t = 0f;
+            while (t < 1f)
+            {
+                Vector3 realPointCoordinates = transform.localToWorldMatrix.MultiplyPoint(_curve.GetPosition(t)) +
+                                               GetTargetOffset();
+                float realRadius = (realPointCoordinates - Target.transform.position.WithY(realPointCoordinates.y))
+                    .magnitude;
+                Vector3 realCenter = Target.transform.position.WithY(realPointCoordinates.y);
+
+                int increment = 15;
+                for (int i = 0; i < 360; i += increment)
+                {
+                    Gizmos.DrawLine(realCenter +
+                                    new Vector3(realRadius * Mathf.Cos(i * Mathf.Deg2Rad),
+                                        0,
+                                        realRadius * Mathf.Sin(i * Mathf.Deg2Rad)),
+                        realCenter +
+                        new Vector3(realRadius * Mathf.Cos((i + increment) * Mathf.Deg2Rad),
+                            0,
+                            realRadius * Mathf.Sin((i + increment) * Mathf.Deg2Rad)));
+                }
+
+                t += tIncrement;
+            }
+
+            // to be sure the last one is drawn
+            {
+                Vector3 realPointCoordinates = transform.localToWorldMatrix.MultiplyPoint(_curve.GetPosition(1)) +
+                                               GetTargetOffset();
+                float realRadius = (realPointCoordinates - Target.transform.position.WithY(realPointCoordinates.y))
+                    .magnitude;
+                Vector3 realCenter = Target.transform.position.WithY(realPointCoordinates.y);
+
+                int increment = 15;
+                for (int i = 0; i < 360; i += increment)
+                {
+                    Gizmos.DrawLine(realCenter +
+                                    new Vector3(realRadius * Mathf.Cos(i * Mathf.Deg2Rad),
+                                        0,
+                                        realRadius * Mathf.Sin(i * Mathf.Deg2Rad)),
+                        realCenter +
+                        new Vector3(realRadius * Mathf.Cos((i + increment) * Mathf.Deg2Rad),
+                            0,
+                            realRadius * Mathf.Sin((i + increment) * Mathf.Deg2Rad)));
+                }
+            }
         }
 
         public Vector3 GetTargetOffset()
         {
             return _target != null ? _target.transform.position - transform.position : Vector3.zero;
+        }
+    }
+
+    public static class Vector3Extensions
+    {
+        public static Vector3 WithY(this Vector3 cVector3, float newY)
+        {
+            return new Vector3(cVector3.x, newY, cVector3.z);
         }
     }
     
