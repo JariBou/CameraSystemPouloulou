c683aa7527deded836748cfb85d00197c290c9c7
Brach <alessandrotano@outlook.fr>
2025-09-19 11:19:02 +0200

    a

---
+++ Assets/_project/Scripts/CameraAddons/CameraColliderChecker.cs
@@ -0,0 +1,33 @@
+using CameraSystem._project.Scripts;
+using UnityEngine;
+
+public class CameraColliderChecker : MonoBehaviour
+{
+    public Transform CamCollider;
+    public float speed = 100.0f;
+    public float treshHoldDistance = 1.0f;
+
+    private CameraConfiguration camConfig = new CameraConfiguration();
+
+    private void Awake()
+    {
+        if (CamCollider == null)
+        {
+            Debug.LogError("CamCollider reference is missing! Destroying component.");
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        float distancetoCollider = Vector3.Distance(transform.position, CamCollider.position);
+
+        Vector3 direction = (transform.position - CamCollider.position).normalized;
+        RaycastHit hit;
+
+        if(Physics.SphereCast(this.transform.position, 3.0f, direction, out hit, 100))
+        {
+            camConfig.pivot = hit.point;
+        }
+    }
+}   
